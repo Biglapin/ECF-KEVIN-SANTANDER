3fa52f3c7f145ef6e4852fa6ad9f78b338bcbcc1
kevin SANTANDER <kevin.santander@protonmail.ch>
2022-03-14 17:45:06 +0100

    Created entity User and register controller

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {}
+
+
+    #[Route('/register', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class)->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+          
+            $user->setPassword($password);
+            
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        } else {
+
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => new Length(60, 2)
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password'
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Password confirmation',
+                'mapped' => false,
+                'attr' => [ 'Please confirm your password']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
