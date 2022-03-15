11d7018625772c9ae3a7a4cdd11b5b25931a98e5
kevin SANTANDER <kevin.santander@protonmail.ch>
2022-03-15 14:22:27 +0100

    installation easyadmin bundle and added crud,add role access for security reason

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Test\FormBuilderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserCrudController extends AbstractCrudController
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {}
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'client' => "ROLE_CLIENT",
+                    'manager' => "ROLE_MANAGER",
+                    'Admin' => 'ROLE_SUPER_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true
+            ]) 
+        ;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        $roles = [ 'ROLE_CLIENT','ROLE_MANAGER','ROLE_SUPER_ADMIN' ];
+        return [
+            TextField::new('firstName'),
+            TextField::new('lastName'),
+            EmailField::new('email'),
+            TextField::new('password')
+                ->setFormType(PasswordType::class)
+                ->hideOnIndex()
+                ->setPermission('ROLE_SUPER_ADMIN'),
+            BooleanField::new('is_admin')
+                ->setPermission('ROLE_SUPER_ADMIN'),
+            ChoiceField::new('roles')
+                ->setChoices(array_combine($roles, $roles))
+                ->allowMultipleChoices()
+                ->renderExpanded()
+                ->renderAsBadges()
+                ->setPermission('ROLE_SUPER_ADMIN')
+        ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword()));
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
+
+}
