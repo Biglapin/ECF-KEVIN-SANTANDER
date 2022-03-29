18ad645daef32d33bac00b01e794d64c3e4fba32
kevin SANTANDER <kevin.santander@protonmail.ch>
2022-03-29 07:33:52 +0200

    bugfixed hotelID returns int instand  of an  object

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -183,7 +183,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getHotelId()
     {
-        return $this->hotelId->getId();
+        return $this->hotelId;
     }
 
     public function setHotelId(?Hotel $hotelId): self
